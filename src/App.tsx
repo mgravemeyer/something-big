4bf278b115e1aa0ea38cc9f59ab56d81f170312b
mgravemeyer <m.gravemeyer@icloud.com>
2021-07-27 00:24:04 +0200

    added RegisterPage style

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React, {useState} from 'react';
 import './App.css';
 import LoginPage from './components/login/LoginPage';
 import RegisterPage from "./components/login/RegisterPage";
+import './style/global.sass';
 
 const App = () => {
   const [login, setLogin] = useState(false);
   const [register, setRegister] = useState(false);
   return (
     <div className="App">
-      { !login && !register && <LoginPage setLogin={setLogin} setRegister={setRegister}/> }
+      {/*{ !login && !register && <LoginPage setLogin={setLogin} setRegister={setRegister}/> }*/}
       {/*{ login && !register && <LoginPage setLogin={setLogin}/> }*/}
-      { !login && register && <RegisterPage setRegister={setRegister}/> }
+      { !login && !register && <RegisterPage setRegister={setRegister}/> }
     </div>
   );
 }
--- src/components/login/RegisterPage.tsx
+++ src/components/login/RegisterPage.tsx
@@ -6,9 +6,30 @@ export interface RegisterPageProps {
 }
 
 const RegisterPage: React.FC<RegisterPageProps> = ( {setRegister}: RegisterPageProps ) => {
+
+    const onSubmit = (e: any) => {
+        e.preventDefault()
+        alert('works')
+        return
+    }
+
     return (
-        <div>
-            <h1> Register </h1>
+        <div className="RegisterForm">
+            <div className="LeftPanel" onSubmit={onSubmit}>
+                <h1> Create your </h1>
+                <h1> account </h1>
+                <form>
+                    <input type="text" placeholder="Full Name"/>
+                    <input type="text" placeholder="E-Mail"/>
+                    <input type="text" placeholder="Password"/>
+                    <p>By clicking "Register" you agree to out Terms and Privacy Policy</p>
+                    <input type='submit' value="Register"/>
+                    <p>Already have an account? Sign In</p>
+                </form>
+            </div>
+            <div className="RightPanel">
+                <p> test </p>
+            </div>
         </div>
     )
 };
